87aa3a69d58576328171e09d45ee1fda4f98a415
justusbersten <justusbersten@gmail.com>
2020-10-25 20:53:14 +0100

    add language option to console.py

--- src/hypermodern_bersten/console.py
+++ src/hypermodern_bersten/console.py
@@ -11,8 +11,14 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
-def main():
+@click.option("-l", "--language", default="en")
+def main(language):
     """The Hypermodern Bersten Python project."""
+    global API_URL
+
+    if language:
+        API_URL = API_URL.replace("en", str(language), 1)
+
     with requests.get(API_URL) as response:
         try:
             response.raise_for_status()
