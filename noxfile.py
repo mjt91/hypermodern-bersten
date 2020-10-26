9e1dbb711ab2b4f6f0b58325a320bfae8e21bf04
justusbersten <justusbersten@gmail.com>
2020-10-26 23:39:44 +0100

    ignore F841 flake8 blame

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,6 @@ import nox
 
 @nox.session(python=["3.9", "3.8"])
 def tests(session):
-    args = session.posargs or ["--cov"]
+    args = session.posargs or ["--cov"]  # noqa: F841
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
