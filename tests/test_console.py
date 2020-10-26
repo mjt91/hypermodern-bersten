f0bcee0e31074d327f3393569ef822ea13827b0b
justusbersten <justusbersten@gmail.com>
2020-10-26 23:13:05 +0100

    turn CliRunner into fixture

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,10 +1,15 @@
 # tests/test_console.py
 import click.testing
+import pytest
 
 from hypermodern_bersten import console
 
 
-def test_main_succeeds():
-    runner = click.testing.CliRunner()
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
