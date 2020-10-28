6dfbabc843201693bfde327f86e6173f9b0b186a
justusbersten <justusbersten@gmail.com>
2020-10-28 07:45:41 +0100

    add end to end testing methods

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,6 @@ import nox
 
 @nox.session(python=["3.9", "3.8"])
 def tests(session):
-    args = session.posargs or ["--cov"]  # noqa: F841
+    args = session.posargs or ["--cov", "-m", "not e2e"]  # noqa: F841
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
