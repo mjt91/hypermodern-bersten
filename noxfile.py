128c759a161252b8a6e525a39a781b6ec64adc71
justusbersten <justusbersten@gmail.com>
2020-11-01 21:42:59 +0100

    add  more nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,10 @@
 # noxfile.py
 import nox
+import tempfile
+
+nox.options.sessions = "lint", "safety", "tests"
+
+locations = "src", "tests", "noxfile.py"
 
 
 @nox.session(python=["3.9", "3.8"])
@@ -7,3 +12,39 @@ def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]  # noqa: F841
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+
+@nox.session(python=["3.9", "3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order"
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.9", "3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.8"])
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
