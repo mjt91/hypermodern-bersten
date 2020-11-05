4da9551430433eb90bd363d393af7547b1c7812c
justusbersten <justusbersten@gmail.com>
2020-11-05 18:02:25 +0100

    add docs skeleton and config file

---
+++ docs/conf.py
@@ -0,0 +1,5 @@
+# docs/conf.py
+"""Sphinx configuration."""
+project = "hypermodern-bersten"
+author = "Marius Theiss"
+copyright = f"2020, {author}"
---
+++ docs/index.rst
@@ -0,0 +1,2 @@
+This is docs/index.rst,
+documenting the Hypermopdern Bersten project.
